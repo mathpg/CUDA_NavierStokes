7ed29141f6e7612856a8fc1e8584682176ca4324
mathpg <matheuspann95@gmail.com>
2021-07-12 20:53:30 -0300

    Correcting bugs

--- NavierStokes_Seq.c
+++ NavierStokes_Seq.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 
 #define N 1000
 #define MIN(x,y) (x<y ? x:y)
@@ -15,7 +16,7 @@ double delx;
 double dely;
 
 //Time Data
-double time;
+double time=0;
 double final_time;
 double del_time;
 double tau; //factor for time step control
@@ -45,6 +46,7 @@ int wW,wE,wN,wS; /*specify the type of boundary condition along the
 					3 for outflow conditions,
 					4 for periodic boundary conditions;*/
 char problem[N];
+struct timeval start, end;
 
 double absf(double a){
 	return a < 0? -1*a : a;
@@ -200,6 +202,33 @@ void read_file(char * file_name){
 	}
 	fclose(fp);
 }
+
+void write_file(char * output){
+	FILE *fp;
+	int i,j;
+	fp = fopen(output, "w"); // write mode
+
+	if (fp == NULL){
+	  perror("Error while opening the file.\n");
+	  exit(EXIT_FAILURE);
+	}
+	fprintf(fp,"Time taken: %ld seconds\n", end.tv_sec-start.tv_sec);
+	for(i=0;i<imax+2;i++){
+		for(j=0;j<jmax+2;j++){			
+			fprintf(fp,"vx[%d][%d]=%f\n",i,j,matrix[i][j].vx);
+			fprintf(fp,"vy[%d][%d]=%f\n",i,j,matrix[i][j].vy);
+			//fprintf(fp,"p[%d][%d]=%f\n",i,j,matrix[i][j].p);
+			//fprintf(fp,"rhs[%d][%d]=%f\n",i,j,matrix[i][j].rhs);
+			//fprintf(fp,"F[%d][%d]=%f\n",i,j,matrix[i][j].F);
+			//fprintf(fp,"G[%d][%d]=%f\n",i,j,matrix[i][j].G);
+			//fprintf(fp,"\n");		
+		}
+		fprintf(fp,"\n");
+	}
+	
+	fclose(fp);
+}
+
 void alocate_matrix(){
 	matrix = (POINT **)malloc((imax+2) * sizeof(POINT*));
 	if(matrix==NULL){
@@ -223,8 +252,8 @@ void free_matrix(){
 }
 void init_UVP(){
 	int i,j;
-	for(i=0;i<imax;i++){
-		for(j=0;j<jmax;j++){
+	for(i=0;i<imax+2;i++){
+		for(j=0;j<jmax+2;j++){
 			matrix[i][j].vx=vx_init;
 			matrix[i][j].vy=vy_init;
 			matrix[i][j].p=p_init;
@@ -252,15 +281,15 @@ void print_matrix(){
 PAIR find_max_vel(){
 	PAIR pair;
 	int i,j;
-	pair.p1=matrix[0][0].vx;
-	pair.p2=matrix[0][0].vy;
-	for(i=0;i<imax;i++){
-		for(j=0;j<jmax;j++){			
-			if(matrix[i][j].vx > pair.p1){
-				pair.p1=matrix[i][j].vx;
+	pair.p1=absf(matrix[0][0].vx);
+	pair.p2=absf(matrix[0][0].vy);
+	for(i=0;i<imax+2;i++){
+		for(j=0;j<jmax+2;j++){			
+			if(absf(matrix[i][j].vx) > pair.p1){
+				pair.p1=absf(matrix[i][j].vx);
 			}
-			if(matrix[i][j].vy > pair.p2){
-				pair.p2=matrix[i][j].vy;
+			if(absf(matrix[i][j].vy) > pair.p2){
+				pair.p2=absf(matrix[i][j].vy);
 			}
 		}
 	}
@@ -272,8 +301,8 @@ void comp_delt(){
 	double aux2;
 	if(tau > 0) {
 		pair=find_max_vel();
-		aux=MIN(delx/pair.p1 ,dely/pair.p2);
-		aux2=MIN((Re/2) * ((delx*delx)+(dely*dely)/delx*delx*dely*dely), aux);
+		aux= MIN((delx/pair.p1) ,(dely/pair.p2));
+		aux2=MIN(aux, ((Re/2)*(SQR(delx)*SQR(dely)/(SQR(delx)+SQR(dely)))));
 		del_time=tau*aux2;
 	}	
 }
@@ -420,6 +449,13 @@ void set_EastBond(){
 	}
 }
 
+void set_lidDrivenCavityProblem(double lid_vel){
+	int i;
+	for(i=1;i<imax+1;i++){
+		matrix[0][i].vx=2.0*lid_vel - matrix[1][i].vx;
+	}
+}
+
 void set_bondCond(){
 	set_NorthBond();
 	set_SouthBond();
@@ -428,77 +464,77 @@ void set_bondCond(){
 }
 
 
-double del_vx_sqr_del_x(double vx_S_point,double vx_C_point,double vx_N_point){ //d(vx²)/dx
+double del_vx_sqr_del_x(double vx_C_point,double vx_W_point,double vx_E_point){ //d(vx²)/dx
 	double aws;
 	
-	aws=(SQR((vx_C_point+vx_N_point)/2) - SQR((vx_S_point+vx_C_point)/2))/delx;
-	aws+=(gam/delx)*((absf(vx_C_point+vx_N_point)/2)*((vx_C_point-vx_N_point)/2) - (absf(vx_S_point+vx_C_point)/2)*((vx_S_point-vx_C_point)/2)) ;
+	aws=(SQR((vx_C_point+vx_E_point)/2) - SQR((vx_W_point+vx_C_point)/2))/delx;
+	aws+=(gam/delx)*((absf(vx_C_point+vx_E_point)/2)*((vx_C_point-vx_E_point)/2) - (absf(vx_W_point+vx_C_point)/2)*((vx_W_point-vx_C_point)/2));
+	return aws;
+}
 
-	return aws;
-}
-double del_vx_vy_del_y(double vx_W_point,double vx_C_point,double vx_E_point, double vy_NW_point, double vy_N_point, double vy_C_point, double vy_W_point){ //d(vx*vy)/dy
+double del_vx_vy_del_y(double vx_N_point,double vx_C_point,double vx_S_point, double vy_C_point, double vy_E_point, double vy_S_point, double vy_SE_point){ //d(vx*vy)/dy
 	double aws;
 	
-	aws=((vy_C_point+vy_N_point/2)*((vx_C_point+vx_W_point)/2) - ((vy_W_point+vy_NW_point)/2)*((vx_W_point+vx_C_point)/2))/dely;
-	aws+=(gam/dely)*((absf((vy_C_point+vy_N_point/2)))*((vx_C_point-vx_W_point)/2) - (absf((vy_W_point+vy_NW_point)/2))*((vx_W_point-vx_C_point)/2));
-	
+	aws=((vy_C_point+vy_E_point)*(vx_C_point+vx_N_point) - (vy_S_point+vy_SE_point)*(vx_S_point+vx_C_point))/2*dely;
+	aws+=(gam/2*dely)*(absf(vy_C_point+vy_E_point)*(vx_C_point-vx_N_point) - absf(vy_S_point+vy_SE_point)*(vx_S_point-vx_C_point));
 	return aws;
 }
-double del_sqr_vx_del_sqr_x(double vx_N_point, double vx_C_point, double vx_S_point){ //d²(vx)/dx²
-	return (vx_N_point-2*vx_C_point+vx_S_point)/SQR(delx);
-}
-double del_sqr_vx_del_sqr_y(double vx_E_point, double vx_C_point, double vx_W_point){ //d²(vx)/dy²
+
+double del_sqr_vx_del_sqr_x(double vx_E_point, double vx_C_point, double vx_W_point){ //d²(vx)/dx²
 	return (vx_E_point-2*vx_C_point+vx_W_point)/SQR(delx);
 }
-double del_vy_sqr_del_y(double vy_C_point, double vy_W_point, double vy_E_point){ // d(vy²)/dy
+
+double del_sqr_vx_del_sqr_y(double vx_N_point, double vx_C_point, double vx_S_point){ //d²(vx)/dy²
+	return (vx_N_point-2*vx_C_point+vx_S_point)/SQR(dely);
+}
+double del_vy_sqr_del_y(double vy_N_point, double vy_C_point, double vy_S_point){ // d(vy²)/dy
 	double aws;
 	
-	aws = (SQR((vy_C_point+vy_E_point)/2) - SQR((vy_W_point+vy_C_point)/2))/dely;
-	aws+=(gam/dely)*(absf((vy_C_point+vy_E_point)/2)*((vy_C_point-vy_E_point)/2) - absf((vy_W_point+vy_C_point)/2)*((vy_W_point-vy_C_point)/2));
+	aws = (SQR((vy_C_point+vy_N_point)/2) - SQR((vy_S_point+vy_C_point)/2))/dely;
+	aws+=(gam/dely)*(absf((vy_C_point+vy_N_point)/2)*((vy_C_point-vy_N_point)/2) - absf((vy_S_point+vy_C_point)/2)*((vy_S_point-vy_C_point)/2));
 	
 	return aws;
 	
 }
-double del_vy_vx_del_x(double vy_S_point,double vy_C_point,double vy_N_point,double vx_C_point,double vx_S_point,double vx_E_point, double vx_SE_point){ //d(vx*vy)/dx
+double del_vy_vx_del_x(double vy_W_point,double vy_C_point,double vy_E_point, double vx_N_point,double vx_C_point,double vx_W_point, double vx_NE_point){ //d(vx*vy)/dx
 	double aws;
 	
-	aws=(((vx_C_point+vx_E_point)/2)*((vy_C_point+vy_N_point)/2) - ((vx_S_point+vx_SE_point)/2)*((vy_S_point+vy_C_point)/2))/delx;
-	aws+=(gam/delx)*(absf((vx_C_point+vx_E_point)/2)*((vy_C_point-vy_N_point)/2) - absf((vx_S_point+vx_SE_point)/2)*((vy_S_point-vy_C_point)/2));
+	aws=((vx_C_point+vx_N_point)*(vy_C_point+vy_E_point) - (vx_W_point+vx_NE_point)*(vy_W_point+vy_C_point))/2*delx;
+	aws+=(gam/2*delx)*(absf(vx_C_point+vx_N_point)*(vy_C_point-vy_E_point) - absf(vx_W_point+vx_NE_point)*(vy_W_point-vy_C_point));
 	return aws;
 }
-double del_sqr_vy_del_sqr_x(double vy_N_point,double vy_C_point,double vy_S_point){//d²(vy)/dx²
-	return (vy_N_point-2*vy_C_point+vy_S_point)/SQR(delx);
+
+double del_sqr_vy_del_sqr_x(double vy_E_point,double vy_C_point,double vy_W_point){//d²(vy)/dx²
+	return (vy_E_point-2*vy_C_point+vy_W_point)/SQR(delx);
 }
-double del_sqr_vy_del_sqr_y(double vy_W_point,double vy_C_point,double vy_E_point){//d²(vy)/dy²
-	return (vy_E_point-2*vy_C_point+vy_W_point)/SQR(dely);
+double del_sqr_vy_del_sqr_y(double vy_N_point,double vy_C_point,double vy_S_point){//d²(vy)/dy²
+	return (vy_N_point-2*vy_C_point+vy_S_point)/SQR(dely);
 }
 
 void comp_FG(){
 	int i,j;
-	for(j=jmax+1;j>=0;j--){
+	for(j=jmax;j>0;j--){
 		for(i=0;i<imax+1;i++){
 			if(i > 0 && i < imax && j < jmax+1 && j > 0){
-				double term1=del_sqr_vx_del_sqr_x(matrix[i-1][j].vx,matrix[i][j].vx,matrix[i+1][j].vx);
-				double term2=del_sqr_vx_del_sqr_y(matrix[i][j+1].vx,matrix[i][j].vx,matrix[i][j-1].vx);
-				double term3=del_vx_sqr_del_x(matrix[i-1][j].vx,matrix[i][j].vx,matrix[i+1][j].vx);
-				double term4=del_vx_vy_del_y(matrix[i][j-1].vx,matrix[i][j].vx,matrix[i][j+1].vx,matrix[i+1][j-1].vy,matrix[i+1][j].vy,matrix[i][j].vy,matrix[i][j-1].vy);
-				matrix[i][j].F=matrix[i][j].vx + del_time*((1/Re)*(term1+term2) - term3 - term4 + gx);
+				double term1=del_sqr_vx_del_sqr_x(matrix[j][i+1].vx, matrix[j][i].vx,matrix[j][i-1].vx);
+				double term2=del_sqr_vx_del_sqr_y(matrix[j+1][i].vx, matrix[j][i].vx,matrix[j-1][i].vx);
+				double term3=del_vx_sqr_del_x(matrix[j][i].vx,matrix[j][i-1].vx,matrix[j][i+1].vx);
+				double term4=del_vx_vy_del_y(matrix[j+1][i].vx,matrix[j-1][i].vx,matrix[j][i].vx, matrix[j][i].vy,matrix[j][i+1].vy,matrix[j-1][i].vy,matrix[j-1][i+1].vy);
+				matrix[j][i].F=matrix[j][i].vx + del_time*(((term1+term2)/Re) - term3 - term4 + gx);
 			}
 			else if(i==0 && j < jmax+1 && j > 0){
 				matrix[j][0].F=matrix[j][0].vx;
-			}else if(i==imax && j < jmax+1 && j > 0){	
-				matrix[j][imax].F=matrix[j][imax].vx;		
+				matrix[j][imax].F=matrix[j][imax].vx;
 			}
 			if(i > 0 && i < imax+1 && j < jmax+1 && j > 1){
-				double term5=del_sqr_vy_del_sqr_x(matrix[i+1][j].vy,matrix[i][j].vy,matrix[i-1][j].vy);
-				double term6=del_sqr_vy_del_sqr_y(matrix[i][j-1].vy,matrix[i][j].vy,matrix[i][j+1].vy);
-				double term7=del_vy_sqr_del_y(matrix[i][j].vy,matrix[i][j-1].vy,matrix[i][j+1].vy);
-				double term8=del_vy_vx_del_x(matrix[i-1][j].vy,matrix[i][j].vy,matrix[i+1][j].vy,matrix[i][j].vx,matrix[i-1][j].vx,matrix[i+1][j].vx,matrix[i+1][j-1].vx);
-				matrix[i][j].G=matrix[i][j].vy + del_time*((1/Re)*(term5+term6) - term7 - term8 + gy);
+				double term5=del_sqr_vy_del_sqr_x(matrix[j][i+1].vy,matrix[j][i].vy,matrix[j][i-1].vy);
+				double term6=del_sqr_vy_del_sqr_y(matrix[j+1][i].vy,matrix[j][i].vy,matrix[j-1][i].vy);
+				double term7=del_vy_vx_del_x(matrix[j][i-1].vy, matrix[j][i].vy,matrix[j][i+1].vy, matrix[j+1][i].vx,matrix[j][i].vx, matrix[j][i-1].vx ,matrix[j+1][i-1].vx);
+				double term8=del_vy_sqr_del_y(matrix[j+1][i].vy ,matrix[j][i].vy,matrix[j-1][i].vy);
+				matrix[j][i].G=matrix[j][i].vy + del_time*(((term5+term6)/Re) - term7 - term8 + gy);
 			}
-			else if(j==jmax+1 && i > 0 && i< imax+1){
+			else if(j==jmax && i > 0 && i< imax+1){
 				matrix[jmax+1][i].G=matrix[jmax+1][i].vy;
-			}else if(j==1 && i > 0 && i < imax+1){
 				matrix[1][i].G=matrix[1][i].vy;
 			}
 		}
@@ -510,7 +546,7 @@ void comp_RHS(){
 	int i,j;
 	for(j=jmax;j>0;j--){
 		for(i=1;i<imax+1;i++){
-			matrix[i][j].rhs=((matrix[i][j].F-matrix[i-1][j].F)/delx +(matrix[i][j].G-matrix[i][j-1].G)/dely)/del_time;
+			matrix[j][i].rhs=((matrix[j][i].F-matrix[j][i-1].F)/delx + (matrix[j][i].G-matrix[j-1][i].G)/dely)/del_time;
 		}
 	}
 }
@@ -521,24 +557,31 @@ int Poisson(){
 	int i,j;
 	double aux;
 	int ew,ee,es,en;
-	double r=1000;
-	while(iter < max_iter || r < eps){
-		for(j=jmax;j>0;j--){
-			ew=i>1? 1:0;
-			ee=i<imax? 1:0;
-			for(i=1;i<imax+1;i++){
-				es=j>1 ? 1:0;
-				en=j<jmax ? 1:0;
-				aux=omg/((ee+ew)/SQR(delx) + (en+es)/SQR(dely));
-				aux=aux*((ee*matrix[i+1][j].p+ ew*matrix[i-1][j].p)/SQR(delx) + (en*matrix[i][j+1].p + es*matrix[i][j-1].p)/SQR(dely) - matrix[i][j].rhs); 
-				matrix[i][j].p=(1-omg)*matrix[i][j].p + aux;
+	double max=0;
+	double diff;
+	while(iter < max_iter){
+		max=0;
+		for(i=1;i<imax+1;i++){
+				ew=i>1?1:0;
+				ee=i<imax?1:0;	
+			for(j=jmax;j>0;j--){						
+				es=j>1?1:0;
+				en=j<jmax?1:0;			
+				aux=omg/((ee+ew)/SQR(delx) + (en+es)/SQR(dely));				
+				aux=aux*((ee*matrix[j][i+1].p + ew*matrix[j][i-1].p)/SQR(delx) + (en*matrix[j+1][i].p + es*matrix[j-1][i].p)/SQR(dely) - matrix[j][i].rhs); 
+				aux=(1-omg)*matrix[j][i].p + aux;
+				diff=absf(matrix[j][i].p-aux);
+				if(diff > max) max=diff;
+				matrix[j][i].p=aux;
 			}
 		
+		}	
+		if(max < eps){
+			return iter;
 		}
 		iter++;
 	}
-
-return 0;
+	return iter;
 }
 
 
@@ -547,10 +590,10 @@ void adap_Vel(){
 	for(j=jmax;j>0;j--){
 		for(i=1;i<imax+1;i++){
 			if(i < imax){
-				matrix[i][j].vx=matrix[i][j].F - (del_time/delx*(matrix[i+1][j].p - matrix[i][j].p));
+				matrix[j][i].vx=matrix[j][i].F - (del_time/delx*(matrix[j][i+1].p - matrix[j][i].p));
 			}
 			if(j < jmax){
-				matrix[i][j].vy=matrix[i][j].G - (del_time/dely*(matrix[i][j+1].p - matrix[i][j].p));
+				matrix[j][i].vy=matrix[j][i].G - (del_time/dely*(matrix[j+1][i].p - matrix[j][i].p));
 			}
 		}
 	}
@@ -559,22 +602,45 @@ void adap_Vel(){
 
 
 int main(int argc, char ** argv){
-
+	
+	int set_time=0;
+	double ant_del_time=0;
+	int num_time=0;
+	int limit_time=100;
 	read_file(argv[1]);
-	//print_values();
 	alocate_matrix();
 	init_UVP();
-	//print_matrix();
-	comp_delt();
-	set_bondCond();
-	comp_FG();
-	comp_RHS();
-	Poisson();
-	adap_Vel();
-	print_matrix();
-
+	gettimeofday(&start, NULL);
+	while(time < final_time){
+		if(!set_time){
+			comp_delt();
+			ant_del_time=del_time;	
+		}
+		if((ant_del_time - del_time) < 0.000001 && !set_time){
+			num_time++;
+			if(num_time==limit_time) set_time=1;
+		}
+		else{
+			num_time=0;
+		}
+		
+		set_bondCond();
+		set_lidDrivenCavityProblem(1.0);
+		comp_FG();
+		comp_RHS();
+		int it=Poisson();
+		adap_Vel();
+		time+=del_time;		
+		printf("deltime=%.10f\n",del_time);
+		printf("time=%f\n",time);
+		printf("settime=%d\n",set_time);
+		printf("iter=%d\n",it);
+	}
+	//set_bondCond();
+	//set_lidDrivenCavityProblem(1.0);
+	gettimeofday(&end, NULL);
+	write_file(argv[2]);
 	free_matrix();
-	printf("%f\n",del_time);
 	return 0;
 }
  
